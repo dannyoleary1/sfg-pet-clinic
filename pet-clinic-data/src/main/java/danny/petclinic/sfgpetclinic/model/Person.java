74efd9addbef50366bd3880c67d76cb67a8484d2
danny o'leary <dannyoleary1@gmail.com>
2019-10-02 15:42:33 +0100

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package danny.petclinic.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package danny.petclinic.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
