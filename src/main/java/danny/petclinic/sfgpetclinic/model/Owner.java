170f4cfe7281e3cf4ee51f9e6f9f4bfdd147e171
danny o'leary <dannyoleary1@gmail.com>
2019-10-02 14:11:59 +0100

    Adding Pojos. Closes #2.

---
+++ src/main/java/danny/petclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package danny.petclinic.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/danny/petclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package danny.petclinic.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
