55bbe3ad997835873c935cf2d1762c4ce2348214
danny o'leary <dannyoleary1@gmail.com>
2019-10-04 16:29:26 +0100

    Added Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package danny.petclinic.sfgpetclinic.services;
+
+import danny.petclinic.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/SpecialtyJpaService.java
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/SpecialtyJpaService.java
@@ -3,10 +3,15 @@ package danny.petclinic.sfgpetclinic.services.springdatajpa;
 import danny.petclinic.sfgpetclinic.model.Speciality;
 import danny.petclinic.sfgpetclinic.repositories.SpecialtyRepository;
 import danny.petclinic.sfgpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Service
+@Profile("springdatajpa")
 public class SpecialtyJpaService implements SpecialityService {
 
     private final SpecialtyRepository specialtyRepository;
