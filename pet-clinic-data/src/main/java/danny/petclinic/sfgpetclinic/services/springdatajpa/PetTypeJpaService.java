84eaf5802b4e3a4a8ab486826cd1d87c1a0354dc
danny o'leary <dannyoleary1@gmail.com>
2019-10-04 15:58:19 +0100

    added pettype to JPA services. Closes #41

---
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/PetTypeJpaService.java
@@ -0,0 +1,44 @@
+package danny.petclinic.sfgpetclinic.services.springdatajpa;
+
+import danny.petclinic.sfgpetclinic.model.PetType;
+import danny.petclinic.sfgpetclinic.repositories.PetTypeRepository;
+import danny.petclinic.sfgpetclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
