5a9001b2d5c5ffefabcfb4d4ab17dafe8108c30c
danny o'leary <dannyoleary1@gmail.com>
2019-10-04 16:06:31 +0100

    Added spring data jpa service for specialty. Closes #42

---
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/SpecialtyJpaService.java
@@ -0,0 +1,44 @@
+package danny.petclinic.sfgpetclinic.services.springdatajpa;
+
+import danny.petclinic.sfgpetclinic.model.Speciality;
+import danny.petclinic.sfgpetclinic.repositories.SpecialtyRepository;
+import danny.petclinic.sfgpetclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialtyJpaService implements SpecialityService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
