f5b3b7e9d7322a7a31dab8f3805d974f07543b92
danny o'leary <dannyoleary1@gmail.com>
2019-10-04 16:01:53 +0100

    added spring data jpa service. Closes #42

--- pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/PetTypeJpaService.java
+++ pet-clinic-data/src/main/java/danny/petclinic/sfgpetclinic/services/springdatajpa/PetTypeJpaService.java
@@ -3,10 +3,14 @@ package danny.petclinic.sfgpetclinic.services.springdatajpa;
 import danny.petclinic.sfgpetclinic.model.PetType;
 import danny.petclinic.sfgpetclinic.repositories.PetTypeRepository;
 import danny.petclinic.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
