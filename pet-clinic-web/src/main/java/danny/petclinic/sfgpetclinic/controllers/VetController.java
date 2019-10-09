5f76e14873ced011f52dafccbb04a1b71b4a8993
danny o'leary <dannyoleary1@gmail.com>
2019-10-09 12:04:31 +0100

    Implement Json End Point. Closes #67

--- pet-clinic-web/src/main/java/danny/petclinic/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/danny/petclinic/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package danny.petclinic.sfgpetclinic.controllers;
 
+import danny.petclinic.sfgpetclinic.model.Vet;
 import danny.petclinic.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
